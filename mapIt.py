cf0b0c13d03ac7b1870fabdd94e0efda39cc09bb
Marzipan786 <41266003+Kenya123@users.noreply.github.com>
2021-07-28 17:08:56 +0100

    Add files via upload

---
+++ mapIt.py
@@ -0,0 +1,13 @@
+#! python3
+#launches a map in the browser using an address from the command line or clip board
+
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    #get address from command line
+    address = ''.join(sys.argv[1:])
+else:
+    #get address from clipboard
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
